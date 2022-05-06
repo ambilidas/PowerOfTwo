6eb289d4a7075bf9c96e59ab74c17d5ad7692814
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 18:53:35 +0530

    Program for printing the table of powers of 2

---
+++ PowerOf2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo
+{
+    internal class PowerOf2
+    {
+        public void tableOfPower2()
+        {
+            Console.WriteLine("Enter the limit N(should be with in 0<=N<31) ");
+            int N=Convert.ToInt32(Console.ReadLine());
+            int p=1;
+            Console.WriteLine("Powers of Two");
+            for (int i = 0; i <= N; i++)
+            {
+                if (i == 0)
+                {
+                    p = 1;
+                    
+                }
+                else
+                {
+                    p = p * 2;
+                   
+                }
+                Console.WriteLine("2^{0} = {1}",i,p);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerOf2 myobj = new PowerOf2();
+            myobj.tableOfPower2();
+
+        }
+    }
+}
